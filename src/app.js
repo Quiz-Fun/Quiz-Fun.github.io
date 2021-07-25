740d24048a217446605811fb75bf1573a3150d9d
Koulinski <emil.k.koulinski@gmail.com>
2021-07-25 18:54:50 +0300

    Added question editor functionality

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
-import * as api from './api/data.js';
+import { render, page, html } from './lib.js';
+import { editorPage } from './views/editor/editor.js';
 
-window.api = api;
+const main = document.getElementById('content');
+
+page('/', decorateContext, editorPage);
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, main);
+    next();
+}
